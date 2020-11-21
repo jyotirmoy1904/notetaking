9e7b3a9356c0e73f47cb8c8eab0d7509e2a2d45d
jyotirmoy1904 <jyotirmoy1904@gmail.com>
2020-11-21 15:45:27 +0530

    View Implemented

--- index.js
+++ index.js
@@ -32,33 +32,30 @@ app.use('/assets',express.static(__dirname+'/public'));
 
 //route for homepage
 app.get('/',(req,res) => {
-    let sql = "SELECT * FROM articles;SELECT DISTINCT SubjectName FROM articles;SELECT ID FROM articles;";
-    let query1 = conn.query(sql,[3,1], (err, results,fields) => {
+    let sql = "SELECT * FROM articles;SELECT DISTINCT SubjectName FROM articles;";
+    let query1 = conn.query(sql,[2,1], (err, results,fields) => {
         if(err)  throw err;
         res.render('Subjects',{
             results: results[0],
             SubjectName: results[1],
-            ViewArticles: results[2]
         });
     });
-    
 });
 //route for dropdown menu
 app.post('/sort',(req,res) => {
-    let sql = "SELECT * FROM articles WHERE SubjectName = '"+req.body.filtering+"';SELECT DISTINCT SubjectName FROM articles;SELECT ID FROM articles WHERE SubjectName= '"+req.body.filtering+"'";
-    let query1 = conn.query(sql,[3,1], (err, results,fields) => {
+    let sql = "SELECT * FROM articles WHERE SubjectName = '"+req.body.filtering+"';SELECT DISTINCT SubjectName FROM articles";
+    let query1 = conn.query(sql,[2,1], (err, results,fields) => {
         if(err)  throw err;
         res.render('Subjects',{
             results: results[0],
             SubjectName: results[1],
-            ViewArticles: results[2]
         });
     });
 });
 //route for new article
 app.post('/save',(req, res) => {
     let date  = new Date();
-    let data = {SubjectName: req.body.SubjectName, Topic: req.body.Topic, CreationDate: date};
+    let data = {SubjectName: req.body.SubjectName, Topic: req.body.Topic, CreationDate: date, Description: req.body.Description};
     let sql = "INSERT INTO articles SET ?";
     let query = conn.query(sql, data, (err, results) => {
         if(err)  throw err;
@@ -66,18 +63,7 @@ app.post('/save',(req, res) => {
         res.redirect('/');
 });
 let cachedValue;
-//route for viewing articles
-app.post('/view',(req,res) => {
-    cachedValue=req.body.viewing;
-    let sql= "SELECT * FROM articles where ID="+cachedValue;
-    let query  = conn.query(sql,(err,results) => {
-        if(err)  throw err;
-        res.render('View',{
-            results:results
-        });
-        
-    });
-});
+
 //route for delete article
 app.post('/delete',(req, res) => {
     let sql = "DELETE FROM articles WHERE ID="+req.body.ID;
